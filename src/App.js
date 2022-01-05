f93591d6f0af588402278e276db2b527a9b38570
salma1389 <charfi.Salma9@gmail.com>
2022-01-05 12:41:33 +0100

    code html to jsx

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@
 import './App.css';
 import myWonderfulImage from "./cat_corona-style.jpg"
 import myWonderfulVideo from "./video/Elissa.mp4"
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
-          <h1 class="title red">Your name here</h1>
+      <div >
+          <h1 className="title red"> Cats Jungle </h1>
           <br />
           <img src="/cat-image.jpg" alt="cat-ima" /> 
           <br />
-          <img src={myWonderfulImage} alt="cat_corona-style" />
+          <img className="cats" src={myWonderfulImage} alt="cat_corona-style" />
+          <img className="cats" src={myWonderfulImage} alt="cat_corona-style" />
           </div>
-          <video width="1000" height="500" controls>
+          <video id="videos" controls>
           <source src={myWonderfulVideo} type="video/mp4" />
           </video>
     </div>
